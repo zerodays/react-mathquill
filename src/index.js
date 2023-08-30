cc1d1609dcd2b935a3730651e539b524c505eac3
Tjaž Silovšek <tjazsilovsek@gmail.com>
2023-08-30 14:28:52 +0200

    feat: use digabi mathquill package

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import style from '@edtr-io/mathquill/build/mathquill.css'
+import style from '@digabi/mathquill/build/mathquill.css'
 
 export function addStyles() {
   if (document.getElementById('react-mathquill-styles') == null) {
--- src/mathquill-loader.js
+++ src/mathquill-loader.js
@@ -1,5 +1,5 @@
 // webpack loaders are used to configure mathquill as a commonjs module, see webpack.config.js
-const MathQuill = require('@edtr-io/mathquill/build/mathquill.js')
+const MathQuill = require('@digabi/mathquill/build/mathquill.js')
 const MQ = MathQuill.getInterface(2)
 
 export default MQ
